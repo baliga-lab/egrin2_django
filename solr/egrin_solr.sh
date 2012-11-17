b7f434db18d369554f1ebc53feee1d3003a52900
Wei-ju Wu <weiju.wu@gmail.com>
2012-11-17 09:07:04 -0800

    Solr 4 configuration added

---
+++ solr/egrin_solr.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# copy this file to $SOLR_HOME/example and execute from there
+java -Dsolr.solr.home=/home/weiju/Projects/ISB/egrin2_django/solr -Dsolr.install.dir=/home/weiju/Development/apache-solr-4.0.0 -jar start.jar
